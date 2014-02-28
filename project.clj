d5c6ea7b708ead8c01d395efb8b024691a6b6d6d
Chris Zheng <chris.zheng@auspost.com.au>
2014-03-01 09:50:56 +1100

    Adding gh-page submodule

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
-(defproject im.chit/purnam.core "0.4.1"
+(defproject im.chit/purnam.core "0.4.3"
   :description "Friendly javascript interop for clojurescript"  
   :url "http://www.github.com/purnam/purnam.core"
   :license {:name "The MIT License"
             :url "http://opensource.org/licencses/MIT"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [im.chit/purnam.common "0.4.1"]]
+                 [im.chit/purnam.common "0.4.3"]]
   :test-paths ["test/clj"]
   :profiles {:dev {:dependencies [[org.clojure/clojurescript "0.0-2138"]
-                                  [im.chit/purnam.test "0.4.1"]
+                                  [im.chit/purnam.test "0.4.3"]
                                   [midje "1.6.0"]]
                    :plugins [[lein-cljsbuild "1.0.0"]
                              [lein-midje "3.1.3"]]}}
